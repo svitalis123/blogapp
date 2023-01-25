32d5e1a0ab733c784c6fd75547493c3a077847c8
vitalis <vitalismutwiri2gmail.com>
2023-01-25 14:11:33 +0300

    Add migration files and Readme

---
+++ db/migrate/20230125105708_add_author_id_to_comment.rb
@@ -0,0 +1,5 @@
+class AddAuthorIdToComment < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :AuthorId, null: false, foreign_key: true
+  end
+end
---
+++ db/migrate/20230125105811_add_post_id_to_comment.rb
@@ -0,0 +1,5 @@
+class AddPostIdToComment < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :PostId, null: false, foreign_key: true
+  end
+end
