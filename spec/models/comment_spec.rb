64b06bfa3ec1705f3dc8405f71c0b495fbf2a326
vitalis <vitalismutwiri2gmail.com>
2023-01-27 01:27:57 +0300

    describe test better

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../rails_helper'
 RSpec.describe Comment, type: :model do
   subject { Comment.new(Text: 'follow the spirit') }
-  before { subject.save }
   it 'expect to respond to the method update_comment_counter' do
     expect(subject).to respond_to(:update_comment_counter)
   end
