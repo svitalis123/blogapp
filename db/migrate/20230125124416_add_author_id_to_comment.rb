7d72814fc971fdf571a5bcb0ecee1ee5edca0593
vitalis <vitalismutwiri2gmail.com>
2023-01-26 15:41:58 +0300

    Process models

--- db/migrate/20230125124416_add_author_id_to_comment.rb
+++ db/migrate/20230125124416_add_author_id_to_comment.rb
@@ -1,5 +1,5 @@
 class AddAuthorIdToComment < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :AuthorId, null: false, foreign_key: true
+    add_reference :comments, :users, null: false, foreign_key: true
   end
 end
