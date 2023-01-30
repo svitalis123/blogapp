fcaec515192a9a6ed1b240d1a836b99a79a9064f
vitalis <vitalismutwiri2gmail.com>
2023-01-30 19:29:32 +0300

    Routing correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :posts, only: [:show, :index]
-  end
+  root 'users#index'
+  get '/users/:id', to: 'users#show'
+  get 'users/:id/posts', to: 'posts#index'
+  get 'users/:id/posts/:post_id', to: 'posts#show'
   # get 'pages/blog'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
