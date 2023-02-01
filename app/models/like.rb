ccef259ae497a2bb67d08e17d695afc6b4557c2b
vitalis <vitalismutwiri2gmail.com>
2023-02-01 23:51:12 +0300

    added forms for Posts, Comments, Likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  after_save :update_like_counter
+  # after_save :update_like_counter
 
   def update_like_counter
     post.increment!(:LikesCounter)
