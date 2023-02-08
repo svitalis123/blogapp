21b0688ee48376f2dd128e4f923e7cfe60576d34
vitalis <vitalismutwiri2gmail.com>
2023-02-08 17:32:58 +0300

    confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
